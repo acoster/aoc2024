0f253f374eb124d17540726f90600f674187c9be
Alex Coster <coster@google.com>
2024-12-06 09:37:56 +0100

    Small refactor. Will finish part 2 when not so busy with my day job...

--- python/p6.py
+++ python/p6.py
@@ -8,6 +8,9 @@ class Direction(Enum):
     LEFT = 4
 
 
+
+
+
 def turn_right(direction: Direction) -> Direction:
     new_direction = {
         Direction.UP: Direction.RIGHT,
@@ -32,35 +35,65 @@ def is_out_of_bounds(i: int, j: int, h: int, w: int) -> bool:
     return i < 0 or i >= h or j < 0 or j >= w
 
 
-def walk(lines : List[AnyStr]) -> int:
-    direction = Direction.UP
-    i, j = 0, 0
-    height = len(lines)
-    width = len(lines[0])
-    for x in range(len(lines)):
-        y = lines[x].find('^')
-        if y != -1:
-            i = x
-            j = y
-            break
-    else:
-        raise Exception('No ^ found in map!')
+class Map(object):
+    def __init__(self, lines: List[AnyStr]):
+        self.lines = lines
+        self.height = len(lines)
+        self.width = len(self.lines[0])
 
-    positions = 0
-    while True:
-        if lines[i][j] != 'X':
-            positions += 1
-        lines[i] = lines[i][:j] + 'X' + lines[i][j+1:]
-        next_i, next_j = next_position(direction, i, j)
-        if is_out_of_bounds(next_i, next_j, height, width):
-            return positions
+        # Starting positions
+        self.i = 0
+        self.j = 0
 
-        if lines[next_i][next_j] == '#':
-            direction = turn_right(direction)
+        for i in range(len(lines)):
+            j = lines[i].find('^')
+            if j != -1:
+                self.i = i
+                self.j = j
+                break
         else:
-            i, j = next_i, next_j
+            raise Exception('No starting position found!')
+
+    def is_out_of_bounds(self, i, j):
+        return i < 0 or i >= self.height or j < 0 or j >= self.width
+
+    def is_obstacle(self, i: int, j: int) -> bool:
+        return self.lines[i][j] == '#'
+
+    def has_obstacles_ahead(self, direction: Direction, i: int, j: int ) -> bool:
+        i, j =  next_position(direction, i, j)
+        while not self.is_out_of_bounds(i, j):
+            if self.lines[i][j] == "#": return True
+            i, j = next_position(direction, i, j)
+        return False
 
 
-puzzle = [x.strip() for x in  open('p6.txt').readlines()]
-result = walk(puzzle)
+    def count_positions(self) -> int:
+        direction = Direction.UP
+        i = self.i
+        j = self.j
+
+        visited = set()
+
+        while True:
+            if (i, j) not in visited:
+                visited.add((i, j))
+
+            next_i, next_j = next_position(direction, i, j)
+            if self.is_out_of_bounds(next_i, next_j):
+                return len(visited)
+
+            if self.is_obstacle(next_i, next_j):
+                direction = turn_right(direction)
+            else:
+                i, j = next_i, next_j
+
+    def count_loops(self) -> int:
+        # Will do once not so busy. TL;DR: for each step, check if there's an
+        # obstacle to the (relative) right, and if so, check if adding an
+        # obstacle ahead causes a loop.
+        return 0
+
+puzzle = Map([x.strip() for x in  open('p6.txt').readlines()])
+result = puzzle.count_positions()
 print(f'Positions visited: {result}')
