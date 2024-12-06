8f5b327a9219a3428d138b3e7cc294586af96417
Alex Coster <coster@google.com>
2024-12-06 14:07:26 +0100

    Minor fixes while I wait something

--- python/p6.py
+++ python/p6.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from typing import List, AnyStr, Tuple, Optional, Set
+from typing import List, AnyStr, Tuple, Optional, Set, Iterator
+from collections import namedtuple
 
 
 class Direction(Enum):
@@ -8,33 +9,28 @@ class Direction(Enum):
     DOWN = 3
     LEFT = 4
 
-
-Coord = Tuple[int, int]
-
-
-def turn_right(direction: Direction) -> Direction:
-    new_direction = {
-        Direction.UP: Direction.RIGHT,
-        Direction.RIGHT: Direction.DOWN,
-        Direction.DOWN: Direction.LEFT,
-        Direction.LEFT: Direction.UP,
-    }
-    return new_direction[direction]
+    def turn(self):
+        new_direction = {
+            Direction.UP: Direction.RIGHT,
+            Direction.RIGHT: Direction.DOWN,
+            Direction.DOWN: Direction.LEFT,
+            Direction.LEFT: Direction.UP,
+        }
+        return new_direction[self]
 
 
-def next_position(direction: Direction, i: int, j: int) -> Coord:
+Coord = namedtuple("Coord", ["i", "j"])
+
+
+def next_position(direction: Direction, p: Coord) -> Coord:
     if direction == Direction.UP:
-        return i - 1, j
+        return Coord(p.i - 1, p.j)
     if direction == Direction.RIGHT:
-        return i, j + 1
+        return Coord(p.i, p.j + 1)
     if direction == Direction.DOWN:
-        return i + 1, j
+        return Coord(p.i + 1, p.j)
     if direction == Direction.LEFT:
-        return i, j - 1
-
-
-def is_out_of_bounds(i: int, j: int, h: int, w: int) -> bool:
-    return i < 0 or i >= h or j < 0 or j >= w
+        return Coord(p.i, p.j - 1)
 
 
 class Map(object):
@@ -49,70 +45,53 @@ class Map(object):
         for i in range(len(lines)):
             j = lines[i].find('^')
             if j != -1:
-                self.start = (i, j)
+                self.start = Coord(i, j)
                 break
         else:
             raise Exception('No starting position found!')
 
-    def is_out_of_bounds(self, i, j):
-        return i < 0 or i >= self.height or j < 0 or j >= self.width
+    def is_out_of_bounds(self, p: Coord) -> bool:
+        return p.i < 0 or p.i >= self.height or p.j < 0 or p.j >= self.width
 
-    def is_obstacle(self, i: int, j: int) -> bool:
-        return not self.is_out_of_bounds(i, j) and self.lines[i][j] == '#'
-
-    def has_obstacles_ahead(self, direction: Direction,
-                            position: Coord) -> bool:
-        i, j = next_position(direction, *position)
-        while not self.is_out_of_bounds(i, j):
-            if self.lines[i][j] == "#": return True
-            i, j = next_position(direction, i, j)
-        return False
+    def is_obstacle(self, p: Coord) -> bool:
+        return not self.is_out_of_bounds(p) and self.lines[p.i][p.j] == '#'
 
     def walk(self, direction: Direction, start: Coord,
-             extra_obstacle: Optional[Coord] = None) -> Tuple[
-        int, int, Direction]:
-        i, j = start
+             extra_obstacle: Optional[Coord] = None) -> Iterator[Tuple[
+        Coord, Direction]]:
+        pos = start
 
         while True:
-            yield i, j, direction
-
-            next_i, next_j = next_position(direction, i, j)
-            if self.is_out_of_bounds(next_i, next_j):
+            yield pos, direction
+            np = next_position(direction, pos)
+            if self.is_out_of_bounds(np):
                 return
 
-            while self.is_obstacle(next_i, next_j) or (
-            next_i, next_j) == extra_obstacle:
-                direction = turn_right(direction)
-                next_i, next_j = next_position(direction, i, j)
+            while self.is_obstacle(np) or np == extra_obstacle:
+                direction = direction.turn()
+                np = next_position(direction, pos)
 
-            i, j = next_i, next_j
+            pos = np
 
     def get_path(self) -> Set[Coord]:
-        direction = Direction.UP
-        visited = set()
-
-        for i, j, direction in self.walk(direction, self.start):
-            if (i, j) not in visited:
-                visited.add((i, j))
-
-        return visited
+        return {x[0] for x in self.walk(Direction.UP, self.start)}
 
     def is_loop(self, obstacle: Coord) -> bool:
         states = set()
-        for i, j, direction in self.walk(Direction.UP, self.start, obstacle):
-            if (i, j, direction) in states:
+        for pos, direction in self.walk(Direction.UP, self.start, obstacle):
+            if (pos, direction) in states:
                 return True
-            states.add((i, j, direction))
+            states.add((pos, direction))
         return False
 
 
+if __name__ == '__main__':
+    puzzle = Map([x.strip() for x in open('p6.txt').readlines()])
+    path = puzzle.get_path()
+    print(f'Positions visited: {len(path)}')
 
-puzzle = Map([x.strip() for x in open('p6.txt').readlines()])
-path = puzzle.get_path()
-print(f'Positions visited: {len(path)}')
-
-loops = 0
-for potential_obstacle in path:
-    if puzzle.is_loop(potential_obstacle):
-        loops += 1
-print(f'Loops: {loops}')
+    loops = 0
+    for potential_obstacle in path:
+        if puzzle.is_loop(potential_obstacle):
+            loops += 1
+    print(f'Loops: {loops}')
