09aff5b2b050216c9b7beca8713b0871a2044d38
Alex Coster <coster@google.com>
2024-12-11 07:16:35 +0100

    Day 11, part 2

--- python/p11.py
+++ python/p11.py
@@ -1,6 +1,7 @@
 from typing import List
-import itertools
+from functools import cache
 
+@cache
 def op(value: int) -> List[int]:
     if value == 0:
         return [1]
@@ -8,33 +9,19 @@ def op(value: int) -> List[int]:
     if len(value_str) % 2 == 0:
         mid = len(value_str) // 2
         return [int(value_str[:mid]), int(value_str[mid:])]
-
     return [2024 * value]
 
-def solve(values: List[int], depth: int) -> int:
-    """Performs an iteration,
+@cache
+def solve(v: int, depth: int):
+    if depth == 0: return 1
+    return sum([solve(x, depth - 1) for x in op(v)])
 
-    >>> solve([125, 17], 1)
-    3
-    >>> solve([125, 17], 25)
-    55312
-    """
-    count = 0
-    for v in values:
-        step = [v]
-        for i in range(depth):
-            step = itertools.chain(*[op(x) for x in step])
-        count += len(list(step))
-
-    return count
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-
     with open('p11.txt') as f:
         stones = [int(x) for x in f.read().strip().split(' ')]
 
-    number_stones = solve(stones, 25)
-    print(f'There are {number_stones} stones.')
+    number_stones_25 = sum([solve(x, 25) for x in stones])
+    number_stones_75 = sum([solve(x, 75) for x in stones])
+    print(f'There are {number_stones_25} and {number_stones_75} stone')
 
