0bf0e7fab3db47e5f3de9e5482f590303b3ca45a
Alex Coster <coster@google.com>
2024-12-14 14:11:05 +0100

    Day 14, part 2, solved with artisan organic neural network pattern recognition

--- python/p14.py
+++ python/p14.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import AnyStr
 from enum import Enum
+from copy import deepcopy
 from collections import defaultdict
 import re
 
@@ -48,20 +49,52 @@ class Robot:
     def __repr__(self) -> str:
         return f'Robot({self.position}, {self.velocity})'
 
-
-if __name__ == '__main__':
-    g = Grid(width=101, height=103)
-    with open('p14.txt') as f:
-        robots = [Robot(l, g) for l in f.readlines()]
-
+def solve_part_1(robots: list[Robot]) -> int:
+    robots = deepcopy(robots)
     frequency_table = defaultdict(int)
     for robot in robots:
         robot.move(100)
         frequency_table[g.get_quadrant(robot.position)] += 1
 
     score = 1
+    if Quadrant.NONE in frequency_table:
+        frequency_table.pop(Quadrant.NONE)
     for quadrant in frequency_table:
-        if quadrant == Quadrant.NONE: continue
         score *= frequency_table[quadrant]
 
+    return score
+
+def print_map(positions):
+    for y in range(103):
+        for x in range(101):
+            if Coord(x, y) in positions:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print()
+
+
+def solve_part_2(robots: list[Robot], iteration_limit: int = 11_000):
+    robots = deepcopy(robots)
+    for i in range(1, iteration_limit):
+        positions = defaultdict(int)
+        has_duplicates = False
+        for robot in robots:
+            robot.move(1)
+            positions[robot.position] += 1
+            if positions[robot.position] > 1:
+                has_duplicates = True
+
+        if has_duplicates: continue
+        print(f'Time elapsed: {i} seconds')
+        print_map(positions.keys())
+
+
+if __name__ == '__main__':
+    g = Grid(width=101, height=103)
+    with open('p14.txt') as f:
+        robots = [Robot(l, g) for l in f.readlines()]
+
+    score = solve_part_1(robots)
     print(f'The score is {score}')
+    solve_part_2(robots)
